cc71b751dcabdf628a04b12cc1f22d146b673f6b
Lakshit Sharma <lakshits5445@gmail.com>
2026-06-17 17:53:47 +0530

    Create list_functions.py

---
+++ list_functions.py
@@ -0,0 +1,15 @@
+# List Functions Example
+
+nums = [15, 7, 25, 3, 40]
+
+print("Minimum Value:", min(nums))
+
+print("Maximum Value:", max(nums))
+
+print("Sum of Elements:", sum(nums))
+
+nums.sort()
+print("Sorted List:", nums)
+
+nums.reverse()
+print("Reversed List:", nums)
