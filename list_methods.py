89d8654e50b5a10039bdf9b6e9dfb855d1f67323
Lakshit Sharma <lakshits5445@gmail.com>
2026-06-17 17:51:35 +0530

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,20 @@
+# Python List Methods Example
+
+fruits = ["Apple", "Banana", "Mango"]
+
+# append()
+fruits.append("Orange")
+print("After Append:", fruits)
+
+# insert()
+fruits.insert(1, "Grapes")
+print("After Insert:", fruits)
+
+# remove()
+fruits.remove("Banana")
+print("After Remove:", fruits)
+
+# pop()
+removed = fruits.pop()
+print("Removed Element:", removed)
+print("After Pop:", fruits)
