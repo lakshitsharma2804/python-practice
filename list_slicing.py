fbab903855540cf4cd464b2cce9d8065f07bdced
Lakshit Sharma <lakshits5445@gmail.com>
2026-06-17 17:52:31 +0530

    Create list_slicing.py

---
+++ list_slicing.py
@@ -0,0 +1,11 @@
+# List Slicing Example
+
+nums = [10, 20, 30, 40, 50]
+
+print("Original List:", nums)
+
+print("First 3 Elements:", nums[:3])
+
+print("Last 2 Elements:", nums[-2:])
+
+print("Middle Elements:", nums[1:4])
